517cdd5bc58828920b9c5a206afac6a67d00799c
Ezypoliorz <ezypoliorz.275@gmail.com>
2026-04-22 17:07:17 +0200

    New version

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ def main() :
 
     start_time = time.perf_counter()
 
-    print("Calling fetching function.")
-    fetch_data.fetch(startIssue=1589, endIssue=1639)
+    print("Calling fetching function for training data.")
+    fetch_data.fetch(startIssue=1589, endIssue=1639, trainingSet=True)
+    print(f"Fetching took {int(time.perf_counter() - start_time)}s")
+    intermediate_time = time.perf_counter()
+
+    print("Calling fetching function for testing data.")
+    fetch_data.fetch(startIssue=1640, endIssue=1641, trainingSet=False)
     print(f"Fetching took {int(time.perf_counter() - start_time)}s")
     intermediate_time = time.perf_counter()
 
